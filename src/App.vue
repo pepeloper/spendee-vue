c13a35cea481087db75c99d6780a5760fbb6fcf7
Pepe García <pgarciag93@gmail.com>
2020-04-30 19:37:01 +0200

    Manipulate data

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,9 @@
           <label for="people" class="block text-sm font-medium text-gray-700">People</label>
           <div class="mt-1 flex rounded-md shadow-sm">
             <button
-              class="-mr-px relative px-4 py-2 border border-gray-300 rounded-l-md text-gray-700 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 z-10">
+              @click="removePerson"
+              class="-mr-px relative px-4 py-2 border border-gray-300 rounded-l-md text-gray-700 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 z-10"
+            >
               <svg fill="none" class="h-5 w-5 text-gray-400" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 14a6 6 0 00-6 6v1h12v-1a6 6 0 00-6-6zM21 12h-6" />
@@ -43,7 +45,10 @@
             <div class="relative flex-grow focus-within:z-20">
               <input id="people" v-model="form.people" class="text-center form-input w-full rounded-none text-sm" placeholder="1" />
             </div>
-            <button class="-ml-px relative px-4 py-2 border border-gray-300 rounded-r-md text-gray-700 focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+            <button
+              @click="addPerson"
+              class="-ml-px relative px-4 py-2 border border-gray-300 rounded-r-md text-gray-700 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
+            >
               <svg class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
@@ -68,22 +73,16 @@
 
         <!-- tip percentage -->
         <div v-show="form.tip" class="w-full md:w-1/2 md:pl-8 mt-5 md:mt-0">
-          <span class="w-full relative z-0 inline-flex shadow-sm">
-            <button type="button"
-              class="relative flex items-center justify-center inline-flex items-center w-1/4 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-              5%
-            </button>
-            <button type="button"
-              class="-ml-px relative flex items-center justify-center inline-flex items-center w-1/4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-              10%
-            </button>
-            <button type="button"
-              class="-ml-px relative flex items-center justify-center inline-flex items-center w-1/4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-              15%
-            </button>
-            <button type="button"
-              class="-ml-px relative flex items-center justify-center inline-flex items-center w-1/4 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-              20%
+          <span class="w-full relative z-0 inline-flex">
+            <button
+              type="button"
+              v-for="(tip, index) in tipOptions"
+              :key="tip.value"
+              class="relative flex items-center justify-center inline-flex items-center w-1/4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+              :class="{ 'rounded-l-md': index === 0, 'rounded-r-md': index === tipOptions.length - 1, '-ml-px': index > 0 }"
+              @click="setTipPercentage(tip.value)"
+            >
+              {{ tip.label }}
             </button>
           </span>
         </div>
@@ -93,7 +92,7 @@
 
       <!-- footer -->
       <div class="mt-8 flex justify-between items-center">
-        <span class="text-gray-500 border-b border-gray-500 text-sm leading-snug">Reset</span>
+        <span @click="resetForm" class="text-gray-500 border-b border-gray-500 text-sm leading-snug">Reset</span>
         <span class="inline-flex rounded-md shadow-sm">
           <button type="button"
             class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
@@ -166,11 +165,40 @@ export default {
         amount: 0,
         people: 1,
         tip: false,
-        tipAmount: '',
+        tipAmount: null,
       },
       showResults: false,
+      tipOptions: [
+        { label: '5%', value: '5' },
+        { label: '10%', value: '10' },
+        { label: '15%', value: '15' },
+        { label: '20%', value: '20' },
+      ],
     }
   },
+
+  methods: {
+    removePerson() {
+      if (this.form.people > 1) {
+        this.form.people--
+      }
+    },
+
+    addPerson() {
+      this.form.people++
+    },
+
+    setTipPercentage(percentage) {
+      this.form.tipAmount = percentage
+    },
+
+    resetForm() {
+      this.form.amount = 0
+      this.form.people = 1
+      this.form.tip = false
+      this.form.tipAmount = null
+    },
+  }
 };
 </script>
 <style>
